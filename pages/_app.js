40b9ce140d1abcde5205c2a485a58c6af38854cf
Parth Panchal <parthpanchal53@gmail.com>
2023-03-12 17:01:02 +0530

    fix : Typo

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ export default function MyApp({ Component, pageProps }) {
                 <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
-                <link ref="icon" href="/images/favicon.ico" />
+                <link rel="icon" href="/images/favicon.ico" />
             </Head >
             <Component {...pageProps} />
         </>
