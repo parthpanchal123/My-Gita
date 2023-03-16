db811119cde2eaaa003e67bfc9b6a818b3806436
Parth Panchal <parthpanchal53@gmail.com>
2023-03-16 10:16:32 +0530

    fix : Favicon icon fix

--- next-seo-config.ts
+++ next-seo-config.ts
@@ -1,7 +1,32 @@
 export const NEXT_SEO_DEFAULT = {
     title: 'Bhagavad Gita',
     description: 'Explore the timeless wisdom of the Bhagavad Gita on our website. Discover the teachings of Lord Krishna and their relevance to modern life. Our comprehensive resources offer insights, commentaries, and translations of this sacred Hindu scripture. Immerse yourself in the transformative messages of the Gita and find inner peace and harmony. Visit us now to begin your journey.',
-
+    additionalLinkTags:
+        [
+            {
+                rel: 'icon',
+                href: '/favicon.ico',
+            },
+            {
+                rel: 'icon',
+                href: '/favicon-32x32.png',
+                sizes: '32x32'
+            },
+            {
+                rel: 'icon',
+                href: '/favicon-16x16.png',
+                sizes: '16x16'
+            },
+            {
+                rel: 'apple-touch-icon',
+                href: '/touch-icon-ipad.jpg',
+                sizes: '76x76'
+            },
+            {
+                rel: 'manifest',
+                href: '/manifest.json'
+            },
+        ],
     openGraph: {
         type: 'website',
         locale: 'en_IE',
@@ -15,5 +40,5 @@ export const NEXT_SEO_DEFAULT = {
     twitter: {
         cardType: 'summary_large_image',
         site: "https://my-gita.vercel.app/"
-    }
+    },
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 export const metadata = {
   viewport: "width=device-width, initial-scale=1.0",
   icons: {
-    icon: '/images/favicon.ico',
-    apple: '/images/apple-icon.png',
+    icon: '/favicon.ico',
+    apple: '/apple-icon.png',
   },
 }
 
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} font-sans`}>
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preload" href="/home-cover.webp" as="image" />
       </head>
