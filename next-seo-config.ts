42487aa03f0017a5bbc02d777e1f8df4c1faa984
parthpanchal123 <parthpanchal53@gmail.com>
2024-06-15 10:44:17 +0530

    seo changes

--- next-seo-config.ts
+++ next-seo-config.ts
@@ -1,44 +1,50 @@
 export const NEXT_SEO_DEFAULT = {
-    title: 'Bhagavad Gita',
-    description: 'Explore the timeless wisdom of the Bhagavad Gita. Discover the teachings of Lord Krishna and their relevance to modern life chapter/verse wise',
-    additionalLinkTags:
-        [
-            {
-                rel: 'icon',
-                href: '/favicon.ico',
-            },
-            {
-                rel: 'icon',
-                href: '/favicon-32x32.png',
-                sizes: '32x32'
-            },
-            {
-                rel: 'icon',
-                href: '/favicon-16x16.png',
-                sizes: '16x16'
-            },
-            {
-                rel: 'apple-touch-icon',
-                href: '/touch-icon-ipad.jpg',
-                sizes: '76x76'
-            },
-            {
-                rel: 'manifest',
-                href: '/manifest.json'
-            },
-        ],
-    openGraph: {
-        type: 'website',
-        locale: 'en_IE',
-        url: 'https://my-gita.vercel.app/',
-        siteName: 'Bhagavad Gita',
-        images: [{
-            url: "https://my-gita.vercel.app/home-cover.webp",
-            alt: "Cover Image"
-        }]
+  title: "Bhagavad Gita",
+  description:
+    "Explore the timeless wisdom of the Bhagavad Gita. Discover the teachings of Lord Krishna and their relevance to modern life chapter/verse wise",
+  canonical: "https://my-gita.vercel.app/",
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
     },
-    twitter: {
-        cardType: 'summary_large_image',
-        site: "https://my-gita.vercel.app/"
+    {
+      rel: "icon",
+      href: "/favicon-32x32.png",
+      sizes: "32x32",
     },
+    {
+      rel: "icon",
+      href: "/favicon-16x16.png",
+      sizes: "16x16",
+    },
+    {
+      rel: "apple-touch-icon",
+      href: "/touch-icon-ipad.jpg",
+      sizes: "76x76",
+    },
+    {
+      rel: "manifest",
+      href: "/manifest.json",
+    },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://my-gita.vercel.app/",
+    siteName: "Bhagavad Gita",
+    title: "Bhagavad Gita",
+    description:
+      "Explore the timeless wisdom of the Bhagavad Gita. Discover the teachings of Lord Krishna and their relevance to modern life chapter/verse wise",
+    images: [
+      {
+        url: "https://my-gita.vercel.app/home-cover.webp",
+        alt: "Cover Image",
+      },
+    ],
+  },
+  twitter: {
+    cardType: "summary_large_image",
+    site: "https://my-gita.vercel.app/",
+  },
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,86 @@
-import ResNavBar from '@/components/ResNavBar'
-import { inter } from '../../utils/fonts/Fonts'
-import './globals.css'
+import ResNavBar from "@/components/ResNavBar";
+import { inter } from "../../utils/fonts/Fonts";
+import "./globals.css";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   viewport: "width=device-width, initial-scale=1.0",
-  keywords: ["Bhagavad Gita", "Hinduism", "Sanskrit", "Lord Krishna", "Arjuna", "Mahabharata", "Sacred texts", "Chapter summaries", "Verse of the day", "Spirituality", "Yoga", "Self-realization", "Karma", "Dharma", "Bhakti", "Jnana", "Vedanta", "Philosophy", "Wisdom", "Eastern religion", "Sanatan Dharma"],
+  title: "Bhagavad Gita",
+  description:
+    "Explore the timeless wisdom of the Bhagavad Gita. Discover the teachings of Lord Krishna and their relevance to modern life chapter/verse wise.",
+  keywords: [
+    "Bhagavad Gita",
+    "Hinduism",
+    "Sanskrit",
+    "Lord Krishna",
+    "Arjuna",
+    "Mahabharata",
+    "Sacred texts",
+    "Chapter summaries",
+    "Verse of the day",
+    "Spirituality",
+    "Yoga",
+    "Self-realization",
+    "Karma",
+    "Dharma",
+    "Bhakti",
+    "Jnana",
+    "Vedanta",
+    "Philosophy",
+    "Wisdom",
+    "Eastern religion",
+    "Sanatan Dharma",
+  ],
   icons: {
-    icon: '/favicon.ico',
-    apple: '/apple-icon.png',
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
   },
-}
+  twitter: {
+    title: "Bhagavad Gita",
+    description: "A collection of summaries of the Bhagavad Gita in English",
+    card: "summary_large_image",
+    site: "https://camo.githubusercontent.com/9a2e87970d1777b98410250c45524ed70bf124f69e14446c7ef88374de9e5778/68747470733a2f2f692e696d6775722e636f6d2f33595945796a732e706e67",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={`${inter.variable} font-sans`}>
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preload" href="/home-cover.webp" as="image" />
-        <meta name="google-site-verification" content="Nn6UdJ1-wZliN0TspEHLehu1aUSKje9FVuaCWFAzJqc" />
+        <meta
+          name="google-site-verification"
+          content="Nn6UdJ1-wZliN0TspEHLehu1aUSKje9FVuaCWFAzJqc"
+        />
       </head>
       <body>
         <ResNavBar />
         {children}
       </body>
     </html>
-  )
+  );
 }
 
-export const dynamicParams = false
+export const dynamicParams = false;
