8e0e8988c8cd199499c22f015594e6d493c2d5ff
parthpanchal123 <parthpanchal53@gmail.com>
2024-06-15 11:03:06 +0530

    Moreeee SEO

--- src/app/verseOfTheDay/head.tsx
+++ src/app/verseOfTheDay/head.tsx
@@ -1,27 +1,33 @@
 // app/profile/head.js
-import { NextSeo } from 'next-seo';
-import { NEXT_SEO_DEFAULT } from '../../../next-seo-config';
-
+import { NextSeo } from "next-seo";
+import { NEXT_SEO_DEFAULT } from "../../../next-seo-config";
 
 export default async function Head() {
-    const updateMeta = {
-        ...NEXT_SEO_DEFAULT,
-        title: 'Bhagavad Gita | Verse Of The Day',
-        description: 'Start your day with inspiration from the Bhagavad Gita. Our website offers a daily dose of wisdom with a randomly generated verse from this ancient Hindu scripture. Discover the profound teachings of Lord Krishna and apply them to your daily life. With our easy-to-use tool, you can access a new verse every day and reflect on its meaning. Join us now and experience the transformative power of the Gita.',
-        openGraph: {
-            type: 'website',
-            locale: 'en_IE',
-            url: 'https://my-gita.vercel.app/verseOfTheDay',
-            siteName: 'Bhagavad Gita',
-            images: [{
-                url: "https://my-gita.vercel.app/home-cover.webp",
-                alt: "Cover Image"
-            }]
+  const updateMeta = {
+    ...NEXT_SEO_DEFAULT,
+    title: "Bhagavad Gita | Verse Of The Day",
+    description:
+      "Start your day with inspiration from the Bhagavad Gita. Our website offers a daily dose of wisdom with a randomly generated verse from this ancient Hindu scripture. Discover the profound teachings of Lord Krishna and apply them to your daily life. With our easy-to-use tool, you can access a new verse every day and reflect on its meaning. Join us now and experience the transformative power of the Gita.",
+    openGraph: {
+      type: "website",
+      locale: "en_IE",
+      url: "https://my-gita.vercel.app/verseOfTheDay",
+      siteName: "Bhagavad Gita",
+      title: "Bhagavad Gita",
+      description:
+        "Start your day with inspiration from the Bhagavad Gita. Our website offers a daily dose of wisdom with a randomly generated verse from this ancient Hindu scripture. Discover the profound teachings of Lord Krishna and apply them to your daily life. With our easy-to-use tool, you can access a new verse every day and reflect on its meaning. Join us now and experience the transformative power of the Gita.",
+      images: [
+        {
+          url: "https://i.imgur.com/vF1c6Kc.png",
+          alt: "Cover Image",
         },
-        twitter: {
-            cardType: 'summary_large_image',
-            site: "https://my-gita.vercel.app/verseOfTheDay"
-        }
-    };
-    return <NextSeo {...updateMeta} useAppDir={true} />;
+      ],
+    },
+    twitter: {
+      cardType: "summary_large_image",
+      site: "https://my-gita.vercel.app/verseOfTheDay",
+      image: "https://i.imgur.com/vF1c6Kc.png",
+    },
+  };
+  return <NextSeo {...updateMeta} useAppDir={true} />;
 }
