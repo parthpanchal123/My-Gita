e53ddc3916eec2db3965e839c78afb4cb18e6745
parthpanchal123 <parthpanchal53@gmail.com>
2024-07-31 09:33:26 +0530

    image optimizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,56 @@
-
-import Link from "next/link"
-import { kalam, inter } from "../../utils/fonts/Fonts"
+import Link from "next/link";
+import { kalam, inter } from "../../utils/fonts/Fonts";
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen min-w-screen bg-neutral-900 p-2">
       <section className="container h-3/5 m-auto drop-shadow-2xl">
-        <div className="mt-2 rounded-md absolute inset-0 bg-cover bg-center	 bg-backdrop-blur" style={{ opacity: "0.7" }}></div>
+        <Image
+          src="/home-cover.webp"
+          style={{ opacity: "0.7", objectFit: "cover" }}
+          className="mt-2 rounded-md absolute inset-0 bg-center	bg-backdrop-blur"
+          alt="Background"
+          layout="fill"
+          priority
+        />
         <div className="flex flex-col absolute inset-0 justify-end items-center">
-          <h1 className={`text-3xl md:text-5xl text-center font-extrabold ${kalam.variable} font-display drop-shadow-2xl text-white t-shadow z-50`}>सम्पूर्ण श्रीमद्‍भगवद्‍गीता</h1>
-          <h1 className="text-2xl md:text-3xl ${inter.variable} font-sans text-center font-extrabold drop-shadow-2xl text-yellow-200 z-50">The Song of God</h1>
+          <h1
+            className={`text-3xl md:text-5xl text-center font-extrabold ${kalam.variable} font-display drop-shadow-2xl text-white t-shadow z-50`}
+          >
+            सम्पूर्ण श्रीमद्‍भगवद्‍गीता
+          </h1>
+          <h1 className="text-2xl md:text-3xl ${inter.variable} font-sans text-center font-extrabold drop-shadow-2xl text-yellow-200 z-50">
+            The Song of God
+          </h1>
         </div>
-      </section >
+      </section>
       <div className="container m-auto max-w-md text-center">
-        <h1 className={`mt-5 text-gray-100 text-base flex ${inter.variable} font-sans font-extrabold`}>The Bhagavad Gita, a revered text of old,
-          A song of divine wisdom that&apos;s forever told,
-          In poetic verses, it reveals life&apos;s deepest truths,
-          Guiding souls towards liberation, breaking ego&apos;s roots.</h1>
-
+        <h1
+          className={`mt-5 text-gray-100 text-base flex ${inter.variable} font-sans font-extrabold`}
+        >
+          The Bhagavad Gita, a revered text of old, A song of divine wisdom
+          that&apos;s forever told, In poetic verses, it reveals life&apos;s
+          deepest truths, Guiding souls towards liberation, breaking ego&apos;s
+          roots.
+        </h1>
       </div>
       <div>
         <div className="flex w-full flex-col backdrop-blur-2xl rounded-md p-4 justify-center items-center">
           <div className="flex gap-5 flex-wrap justify-center items-center">
-            <Link href={"/verseOfTheDay"}><button className="bg-gradient-to-t from-orange-500 via-orange-300 to-amber-400 text-black font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out">Verse Of The Day</button></Link>
-            <Link href={"/chapter"}><button className="bg-gradient-to-t from-orange-500 via-orange-300 to-amber-400 text-black font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out">Chapter Wise</button></Link>
+            <Link href={"/verseOfTheDay"}>
+              <button className="bg-gradient-to-t from-orange-500 via-orange-300 to-amber-400 text-black font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out">
+                Verse Of The Day
+              </button>
+            </Link>
+            <Link href={"/chapter"}>
+              <button className="bg-gradient-to-t from-orange-500 via-orange-300 to-amber-400 text-black font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out">
+                Chapter Wise
+              </button>
+            </Link>
             {/* <button className="bg-gradient-to-t from-orange-500 via-orange-300 to-amber-400 text-black font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out">Specific Shlok</button> */}
           </div>
         </div>
       </div>
     </div>
-
-  )
+  );
 }
